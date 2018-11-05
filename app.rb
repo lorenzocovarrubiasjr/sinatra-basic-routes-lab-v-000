ed90f9feddb85909bcf4db41174c4878a4d52c67
Lorenzo Covarrubias Jr <lorenzocovarrubiasjr@gmail.com>
2018-11-05 00:54:34 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -6,4 +6,16 @@ class App < Sinatra::Base
     "Hello, World!"
   end 
   
+  get '/name' do
+    "My name is __"
+  end 
+  
+  get '/hometown' do
+    "My hometown is __"
+  end 
+  
+  get '/favorite-song' do 
+    "My favorite song is __"
+  end 
+  
 end
